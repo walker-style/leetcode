456191272af77445410c2ff50bc03da45fc7592a
Jonathan Walker <w@lker.dev>
2024-03-12 11:50:08 +0900

    work in progress daily

--- src/daily.py
+++ src/daily.py
@@ -3,11 +3,74 @@ Place to complete leetcode challenges.
 https://leetcode.com/walker-style/
 """
 
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        arr = [self.val]
+        while self.next:
+            self = self.next
+            arr.append(self.val)
+        return str(arr)
+
+class LinkedListFactory:
+    @staticmethod
+    def create_from_array(array):
+        if not array:
+            return None
+
+        head = ListNode(array[0])
+        current_node = head
+        for val in array[1:]:
+            current_node.next = ListNode(val)
+            current_node = current_node.next
+
+        return head
+
 
 class Daily:  # pylint: disable=too-few-public-methods
     """
     Class to house daily leetcode challenges
     """
+
+    def remove_zero_sum_sublist(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Remove consecutive sequencenses of nodes that sum to zero
+
+        Args:
+            head Optional[ListNode]: node from which to start checking for zero sum sequences
+
+        Return:
+           Optional[ListNode]: list with no consecutive zero sum nodes 
+
+        >>> leet = Daily()
+        >>> head = LinkedListFactory.create_from_array([1,2,-3,3,1]) 
+        >>> leet.remove_zero_sum_sublist(head = head)
+        [3, 1]
+
+        >>> head = LinkedListFactory.create_from_array([1,2,3,-3,4]) 
+        >>> leet.remove_zero_sum_sublist(head = head)
+        [1, 2, 4]
+
+        >>> head = LinkedListFactory.create_from_array([1,2,3,-3,-2])
+        >>> leet.remove_zero_sum_sublist(head = head)
+        [1]
+        """
+
+
+        
+        if not head:
+            return head
+        
+        result = None
+
+            
+        return result
 
     def custom_sort_string(self, order: str, s: str) -> str:
         """
